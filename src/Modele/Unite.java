48238bc334139a77ce9bc57293129aa0fc94fd9b
Preuce <borice.lipant@gmail.com>
2022-02-07 16:10:21 +0100

    cc

--- src/Modele/Unite.java
+++ src/Modele/Unite.java
@@ -1,9 +1,45 @@
 package Modele;
 
+import java.util.ArrayList;
+
 public class Unite {
     public int x, y; //les coordonnées du péons
+    public ArrayList<Integer> inventaire = new ArrayList<>(1); //pour l'instant
 
-    public Unite(){
+    public Unite(int x, int y){
+        this.x = x;
+        this.y = y;
 
+        for(int i = 0; i <= this.inventaire.size();i++){
+            this.inventaire.set(i, 0);
+        }
+    }
+
+}
+public class Paysan {
+
+    public void planteFleur() {
+
+    }
+
+    public void recolterRessource(Ressource r) {
+
+    }
+
+    public void acheterGraine() {
+
+    }
+
+    public void vendre(Ressource r) { //augmenter
+        Bat_Principal.tirelire += r.;
+
+        this.inventaire.get(r.id)--
+    }
+
+    public void avance(){
+
+    }
+
+    public void effrayer(){
     }
 }
