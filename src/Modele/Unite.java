6f7b6a50ec19b69aa516c786b32e26db75a94f75
Preuce <borice.lipant@gmail.com>
2022-02-14 14:49:18 +0100

    Maj BatPrincipal

--- src/Modele/Unite.java
+++ src/Modele/Unite.java
@@ -1,8 +1,5 @@
 package Modele;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 
@@ -21,59 +18,8 @@ public class Unite {
      * @param dir la direction à suivre
      */
     public void avance(double dir){
-        this.x += cos(dir)*Paysan.vitesse;
-        this.y += sin(dir)*Paysan.vitesse;
+        this.x += cos(dir)* Jardinier.vitesse;
+        this.y += sin(dir)* Jardinier.vitesse;
     }
 
-}
-public class Paysan extends Unite{
-    public int[] inventaire = new int[1]; //pour l'instant
-
-    public Paysan(int x, int y){
-        super(x, y);
-        vitesse = 100;
-        Arrays.fill(inventaire, 0);
-    }
-
-    /**
-     * planteFleur
-     * Le paysan plante une fleur sur une case proche
-     */
-    public void planteFleur() {
-        this.inventaire[0]--;
-    }
-
-    /**
-     * recolterRessources
-     * recolte une certaine quantité d'une ressource
-     * @param r la ressource récolter
-     * @param amount la quantité à mettre dans l'inventaire du paysan
-     */
-    public void recolterRessource(Ressource r, int amount) {
-        this.inventaire[r.id] += amount;
-    }
-
-    /**
-     * acheterGraine
-     * Achete une(des) graines
-     * @param amount la quantité acheter
-     */
-    public void acheterGraine(int amount) {
-        BatPrincipal.setTirelire(BatPrincipal.getTirelire()-amount*Fleur.prix);
-    }
-
-    /**
-     * vendre
-     * Vend une certaine quantité d'une ressource
-     * @param r la ressource à vendre
-     * @param amount la quantité à vendre
-     */
-    public void vendre(Ressource r, int amount) { //augmenter
-        BatPrincipal.setTirelire(BatPrincipal.getTirelire()+amount*Fleur.prix);
-        this.inventaire[r.id] -= amount;
-    }
-
-    /*public void effrayer(){
-
-    }*/
 }
