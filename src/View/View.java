8a633344fb32ec05d709ce2e82afe013d5f5980f
nnhanus <noemie.hanus@laposte.net>
2022-02-14 14:36:54 +0100

    petipoulp

--- src/View/View.java
+++ src/View/View.java
@@ -33,8 +33,9 @@ public class View extends JFrame {
         /**ajout image*/
         BufferedImage meduse = null;
         try {/**Le chemin d'acces est spécifique à mon ordinateur et doit être changer si utiliser sur un autre pc*/
-            meduse = ImageIO.read(new File("Image/potipoulp.png"));
+            meduse = ImageIO.read(new File("src/View/Image/potipoulp.png"));
         } catch (IOException e) {
+            System.out.println("Fichier manquant");
         }
 
         JLabel cubomeduse = new JLabel(new ImageIcon(meduse));
