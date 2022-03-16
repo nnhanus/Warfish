e3c10eff9626d47573777ffe7557817832ac8e93
nnhanus <noemie.hanus@laposte.net>
2022-03-16 12:30:42 +0100

    les fleurs sont des fleurs wow et ya bourgeon et fleurs <3

--- src/View/Movable.java
+++ src/View/Movable.java
@@ -15,6 +15,8 @@ public class Movable{
     public static JardinierView jardvue;
     public VueFleur vuefleur;
     private BufferedImage meduse = null;
+    private BufferedImage f1 = null;
+    private BufferedImage B1 = null;
 
     public Movable(JardinierView jardvue, VueFleur vuefleur) {
         this.vuefleur = vuefleur;
@@ -24,14 +26,29 @@ public class Movable{
         } catch (IOException ex) {
             System.out.println("Fichier manquant"); //absence de l'image
         }
+        try {
+            f1 = ImageIO.read(new File("src/View/Image/fleur_rouge.png")); //image de méduse
+        } catch (IOException ex) {
+            System.out.println("Fichier manquant"); //absence de l'image
+        }
+        try {
+            B1 = ImageIO.read(new File("src/View/Image/bourgeon_rouge.png")); //image de méduse
+        } catch (IOException ex) {
+            System.out.println("Fichier manquant"); //absence de l'image
+        }
     }
 
 
     public void paint(Graphics g) {
         for (Fleur f : vuefleur.fleurs) {
             if (f.getEstLa()) {
-                g.setColor(Color.yellow);
-                g.fillRect(f.getX(), f.getY(), 40, 40);
+                //g.setColor(Color.yellow);
+                //g.fillRect(f.getX(), f.getY(), 40, 40);
+                if (f.lifespan >= 450){
+                    g.drawImage(B1, f.getX(), f.getY(), 40, 40, null);
+                } else {
+                    g.drawImage(f1, f.getX(), f.getY(), 40, 40, null);
+                }
             }
         }
         for (Jardinier jardinier : jardvue.listjardinier) {
